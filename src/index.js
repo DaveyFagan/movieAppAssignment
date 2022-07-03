68b386a0749400471684cd70d15319c19eb3e6e7
David Fagan <david.fagan1989@gmail.com>
2022-07-03 16:26:50 +0100

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
 
 const App = () => {
   return (
     <BrowserRouter>
       <SiteHeader />      {/* New Header  */}
+
       <Routes>
       <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
-        />
+        <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}/>
+        <Route path="/movies/upcomingmovies" element={<UpcomingMoviesPage/>}/>
         <Route path="/movies/:id" element={<MoviePage/>} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
